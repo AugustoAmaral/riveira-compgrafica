d6d083b6baed07b6f2b08064e22129a549773f70
Augusto Amaral Pereira <guto_wow@hotmail.com>
2022-04-06 11:35:48 -0300

    Added drawaxed function

--- trabalho_1/trabalho.c
+++ trabalho_1/trabalho.c
@@ -11,6 +11,9 @@ GLenum doubleBuffer;
 #define TITULO_TELA "Trabalho do riveira"
 #define COR_DE_FUNDO 1.0, 1.0, 1.0, 0.0
 
+#define GROSSURA_DA_LINHA_DO_EIXO 1
+#define COR_DA_LINHA 1.0, 0.0, 0.0
+
 // Funçao que redesenha o polígono dentro da tela
 void Reshape(void)
 {
@@ -21,10 +24,32 @@ void Reshape(void)
     glMatrixMode(GL_MODELVIEW);
 }
 
+void DesenharEixos(void)
+{
+	// Grossura da linha do eixo
+	glLineWidth(GROSSURA_DA_LINHA_DO_EIXO);
+	// Cor da linha do eixo
+	glColor3f(COR_DA_LINHA);
+
+	// Desenha o eixo X
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(-LARGURA_TELA_X, 0);
+	glVertex2f(LARGURA_TELA_X, 0);
+	glEnd();
+
+	// Desenha o eixo Y
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(0, -ALTURA_TELA_Y);
+	glVertex2f(0, ALTURA_TELA_Y);
+	glEnd();
+}
+
 static void PreencherTela(void)
 {
     glClearColor(COR_DE_FUNDO);
     glClear(GL_COLOR_BUFFER_BIT);
+
+	DesenharEixos(); 
 
     if (doubleBuffer)
         glutSwapBuffers();
